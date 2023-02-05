37364ad8d70c8a599d696e13748182fb6202ae3b
Andhika Prasetya <andhikapr081@gmail.com>
2023-02-05 16:18:08 +0700

    commit another change to apply env variabel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { useState, useEffect } from "react";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
-import BottomNav from "../components/BottomNav";
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import BottomNav from '../components/BottomNav';
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -18,9 +18,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <GoogleOAuthProvider
-      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
-    >
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <div className="xl:w-[1200px] md:m-auto overflow-hidden h-[100vh]">
         <Navbar />
         <div className="flex gap-6 md:gap-20">
